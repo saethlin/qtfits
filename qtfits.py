5a49515064de5775c19f09dcdc5aaff764a2a27e
saethlin <kimockb@gmail.com>
2016-07-06 01:47:16 -0400

    integrating header display

--- qtfits.py
+++ qtfits.py
@@ -4,17 +4,17 @@ Histogram sliders
 Header display
 Toolbar?
 """
-
-import os
+import re
 import numpy as np
 from astropy.io import fits
-from PyQt5.QtWidgets import QWidget, QGridLayout, QApplication
+from PyQt5.QtWidgets import QWidget, QGridLayout, QApplication, QPlainTextEdit
 from PyQt5.QtCore import Qt
 
 from dirlist import DirList
 from histogram import ImageHistogram
 from imagedisplay import ImageDisplay
 from minimap import MiniMap
+from headerdsiplay import HeaderDisplay
 
 
 class QtFits(QWidget):
@@ -32,12 +32,12 @@ class QtFits(QWidget):
         grid.addWidget(self.mini, 0, 1)
 
         self.main = ImageDisplay(self.mini)
-        grid.addWidget(self.main, 0, 0, 0, 1)
+        grid.addWidget(self.main, 0, 0, 2, 1)
 
-        self.box = DirList(self, os.getcwd())
+        self.box = DirList(self)
         grid.addWidget(self.box, 1, 1, 2, 1)
 
-        self.histogram = ImageHistogram()
+        self.histogram = ImageHistogram(self.main)
         grid.addWidget(self.histogram, 2, 0)
 
         self.handlers = dict()
@@ -50,14 +50,22 @@ class QtFits(QWidget):
         self.handlers[Qt.Key_Right] = self.box.select
         self.handlers[Qt.Key_Backspace] = self.box.back
         self.handlers[Qt.Key_Left] = self.box.back
+        self.handlers[Qt.Key_H] = self.show_header
 
         self.open('test.fits')
 
     def open(self, path, hdu=0):
         with open(path, 'rb') as input_file:
-            image = fits.open(input_file)[hdu].data.astype(np.float32)
+            hdu = fits.open(input_file)[hdu]
+            image = hdu.data.astype(np.float32)
         self.main.image = image
         self.histogram.image = image
+        header_text = str(hdu.header).strip()
+        self.header = re.sub("(.{80})", "\\1\n", header_text, 0, re.DOTALL).strip()
+
+    def show_header(self):
+        window = HeaderDisplay(self.header)
+        window.exec_()
 
     def keyPressEvent(self, event):
         if event.key() in self.handlers:
